a5f0b1829a8ff178ef555653b5a10403f539c50f
Bedir Bayar <bedirbayarfbh@gmail.com>
2024-10-24 16:09:23 +0300

    Bearer authentication in ApiGateway, private network for other apis

---
+++ APIGateway/Controllers/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace APIGateway.Controllers.Middlewares
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // İstek bilgilerini loglama
+            _logger.LogInformation($"Incoming request: {JsonSerializer.Serialize(context.Request.Headers)}");
+
+            // İsteği bir sonraki middleware'e ilet
+            await _next(context);
+        }
+    }
+}
--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,5 +1,10 @@
+using APIGateway.Controllers.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Values;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +14,59 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddAuthentication()
+//    .AddJwtBearer("Bearer", options =>
+//    {
+//        options.RequireHttpsMetadata = false; // Dev için, prod'da true yapýn
+//        options.Audience = "SingleOrDefault"; // Bu deðer JWT'de olmalý
+//    });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.SaveToken = false;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidIssuer = "SingleOrDefault",
+            ValidAudience = "SingleOrDefault",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("2D4A614E645267556B58703273357638792F423F4428472B4B6250655368566D"))
+        };
+        o.Events = new JwtBearerEvents()
+        {
+            //OnAuthenticationFailed = c =>
+            //{
+            //    c.NoResult();
+            //    c.Response.StatusCode = 500;
+            //    c.Response.ContentType = "text/plain";
+            //    return c.Response.WriteAsync(c.Exception.ToString());
+            //},
+            OnChallenge = context =>
+            {
+                context.HandleResponse();
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/json";
+                var result = "You are not Authorized";
+                return context.Response.WriteAsync(result);
+            },
+            OnForbidden = context =>
+            {
+                context.Response.StatusCode = 403;
+                context.Response.ContentType = "application/json";
+                var result = "You are not authorized to access this resource";
+                return context.Response.WriteAsync(result);
+            },
+        };
+    });
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
@@ -22,9 +80,11 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseMiddleware<RequestLoggingMiddleware>();
 await app.UseOcelot();
 
-app.UseAuthorization();
 
 app.MapControllers();
 
